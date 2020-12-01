a087db6a03af0270bead02988e69f083cf41b378
zhangjr <2726822237@qq.com>
2020-12-01 19:40:40 +0800

    添加Git测试类2

---
+++ src/test/java/com/cn/GitTest.java
@@ -0,0 +1,20 @@
+package com.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+public class GitTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("初始化Git创库");
+        System.out.println("zjr");
+        int a = 10;
+        a = a + 2;
+        System.out.println(a);
+
+    }
+
+}
